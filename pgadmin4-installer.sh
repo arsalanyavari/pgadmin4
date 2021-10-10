9b1dd8521e15905699e78c663d2bf2a65fde25df
navid <navidnasyry@gmail.com>
2021-10-10 14:47:00 +0330

    write some comments in shell script

--- pgadmin4-installer.sh
+++ pgadmin4-installer.sh
@@ -1,21 +1,29 @@
 #! /bin/bash
 
+#try to install Dependencies
 sudo apt-get -y install python3 pip python3-venv 2> /dev/null
 sudo pacman -Syu python3 python-pip python-virtualenv 2> /dev/null
 sudo dnf -y install python3 python-pip python3-virtualenv 2> /dev/null
 
+#Create the necessary files
 mkdir /var/lib/pgadmin
 mkdir /var/log/pgadmin
+
+#Change owner of files
 chown $USER /var/lib/pgadmin
 chown $USER /var/log/pgadmin
 
+#Create main dierctory
 cd ~
 mkdir .pgadmin4
 cd .pgadmin4
+
+
 python3 -m venv pgadmin4
 source pgadmin4/bin/activate
 pip install pgadmin4
 
+#Add path of pgadmin to PATH
 shells=`ls ~ -a | grep rc$`
 
 for i in $shells
